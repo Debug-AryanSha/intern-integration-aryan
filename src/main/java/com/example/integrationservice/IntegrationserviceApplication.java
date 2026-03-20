972ecd6ed66d224a5654a1d491aab21877fea6f0
Aryan <aryan@chandikainnov.com>
2026-03-20 13:03:08 +0530

    Initialized folder structure

---
+++ src/main/java/com/example/integrationservice/IntegrationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.integrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationserviceApplication.class, args);
+	}
+
+}
